174755766407747d5a4f1d1b35c9c3552755ca20
jshqhxm <jshqhxm@gmail.com>
2017-09-15 22:51:10 +0800

    Out Put  test  step

--- src/index.js
+++ src/index.js
@@ -3,21 +3,30 @@ import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
 
+import printMe from './print.js';
+
 
 function component() {
 
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
 
-    //add the image to our existing div.
-    var myIcon = new Image();
-    myIcon.src = Icon;
-    element.appendChild(myIcon);
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
 
-    console.log(Data);
+    element.appendChild(btn);
+
+
+    //add the image to our existing div.
+    // var myIcon = new Image();
+    // myIcon.src = Icon;
+    // element.appendChild(myIcon);
+
+    // console.log(Data);
 
     return element;
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    // entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            title:'Output Managment'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        // filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
     module: {
