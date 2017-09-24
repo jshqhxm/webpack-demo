10ae3b512ae3d7cd686872d9f79ff7bf4233d452
jshqhxm <jshqhxm@gmail.com>
2017-09-24 23:17:01 +0800

    prod  demo

--- src/index.js
+++ src/index.js
@@ -1,25 +1,38 @@
-import _ from 'lodash';
+// import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
 import Data from './data.xml';
 
 import printMe from './print.js';
 
+import { cube } from './math.js'
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Looks like we are in development mode!');
+}
+
 
 function component() {
 
-    var element = document.createElement('div');
-    var btn = document.createElement('button');
+    // var element = document.createElement('div');
+    // var btn = document.createElement('button');
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
+    // // Lodash, now imported by this script
+    // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    // element.classList.add('hello');
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+    // btn.innerHTML = 'Click me and check the console!';
+    // btn.onclick = printMe;
 
-    element.appendChild(btn);
+    // element.appendChild(btn);
+    // 
 
+    var element = document.createElement('pre');
+
+    element.innerHTML = [
+        'Hello webpack!',
+        '5 cubed is equal to ' + cube(5)
+    ].join('\n\n');
 
     //add the image to our existing div.
     // var myIcon = new Image();
@@ -32,19 +45,19 @@ function component() {
 
 }
 
-// document.body.appendChild(component());
+document.body.appendChild(component());
 
 
-let element = component(); // Store the element to re-render on print.js changes
-document.body.appendChild(element);
+// let element = component(); // Store the element to re-render on print.js changes
+// document.body.appendChild(element);
 
-if (module.hot) {
-    module.hot.accept('./print.js',function(){
-        console.log('Accepting the updated printMe module!');
-        // printMe();
-        document.body.removeChild(element);
-        element = component(); //Re-render the "component" to update the click handler
-        document.body.appendChild(element);
+// if (module.hot) {
+//     module.hot.accept('./print.js', function() {
+//         console.log('Accepting the updated printMe module!');
+//         // printMe();
+//         document.body.removeChild(element);
+//         element = component(); //Re-render the "component" to update the click handler
+//         document.body.appendChild(element);
 
-    })
-}
+//     })
+// }
